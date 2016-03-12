c99abc38e4eb32a6ab68b8f67525439f304ce05a
Shayan123456 <shayan_jon123@yahoo.com>
2016-03-12 18:27:53 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -229,27 +229,8 @@ function create_config( )
     },
     sudo_users = {87160007},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v4
-An advanced administration bot based on TG-CLI written in Lua
-
-https://github.com/SEEDTEAM/TeleSeed
-
-Admins
-@Empero [Founder]
-@imandaneshi [Developer]
-@POTUS [Developer]
-@seyedan25 [Manager]
-@aRandomStranger [Admin]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
-Our channels
-@teleseedch [English]
-@iranseed [persian]
+    about_text = [[S.H.O.S Bot version5.2 Supergp (BEAUTIFULLY MODE :)))
+    Developer:@shayan123hacker
 ]],
     help_text_realm = [[
 Realm Commands:

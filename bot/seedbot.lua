6803376c2605aa455328d521a2a293adf7a3ffe7
Shayan123456 <shayan_jon123@yahoo.com>
2016-03-15 18:02:11 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -240,10 +240,10 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {87160007},--Sudo users
+    sudo_users = {87160007,137791771},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[S.H.O.S bot Version 5.2.1 Developer: @shayan123hacker
-    Sudo Users : @shayan123hacker & @crazyboy10
+    about_text = [[KiNg bot Version 5.2.1 Developer: @shayan123hacker
+    Sudo Users : @shayan123hacker & @mehdijokers
     ]],
     help_text_realm = [[
 Realm Commands:

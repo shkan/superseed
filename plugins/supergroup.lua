e6b1e3b9612a68556252bfdda5442c0cc12c60a1
Shayan123456 <shayan_jon123@yahoo.com>
2016-03-15 09:59:37 +0330

    Update supergroup.lua

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -1899,7 +1899,7 @@ local function run(msg, matches)
 		end
 
 		if matches[1] == 'help' and not is_owner(msg) then
-			text = "Message /superhelp to @amperTG in private for SuperGroup help"
+			text = "Message /superhelp to @Anti_Spam_Shayan123_Bot2 in private for SuperGroup help"
 			reply_msg(msg.id, text, ok_cb, false)
 		elseif matches[1] == 'help' and is_owner(msg) then
 			local name_log = user_print_name(msg.from)

8d9a76aeb6cdb063cc79873cc2438a8069209df8
Shayan123456 <shayan_jon123@yahoo.com>
2016-03-16 14:03:54 +0330

    Create myid.lua

---
+++ plugins/myid.lua
@@ -0,0 +1,12 @@
+local function run(msg, matches ) 
+  if matches[1] == "myid" then
+    return "your id😐👇🏻\n "..msg.from.id
+  end
+end
+
+return {
+  patterns ={
+    "^[/!#](myid)$" 
+ }, 
+  run = run 
+}
